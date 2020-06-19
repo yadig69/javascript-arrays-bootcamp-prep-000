21184ef0f54193d7319995ceb53e0f58b77b484d
Gregory Singleton <yadig69design@gmail.com>
2020-06-19 03:46:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,6 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 
 var addElementToBeginningOfArray = (arr, val) => {
-  var newArray = [val, ...arr] 
+  var newArray = [val, ...arr]
   return newArray
 }
