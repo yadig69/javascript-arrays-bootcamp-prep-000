1790d6ae89c749c13e4f1244e8aa03416da482d7
Gregory Singleton <yadig69design@gmail.com>
2020-06-19 03:54:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,3 +5,8 @@ var addElementToBeginningOfArray = (arr, val) => {
   var newArray = [val, ...arr]
   return newArray
 }
+
+var destructivelyAddElementToBeginningOfArray = (arr, val) => {
+  arr.unshift(val)
+  return arr 
+}
